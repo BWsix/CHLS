3b3a307cfbccadbf65e7a3a1d59898c2ae4e9825
BWsix <itisbw62@gmail.com>
2021-08-15 22:37:19 +0800

    fix: added a lock to stop `useNewsList` hook from fetching news list twice.

--- src/components/newsList/useGetNewsList.ts
+++ src/components/newsList/useGetNewsList.ts
@@ -2,17 +2,14 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 
 export const useGetNewsList = (uid: string) => {
+  const [prevUid, setPrevUid] = useState("");
+
   const [page, setPage] = useState(0);
   const [newsList, setNewsList] = useState<News[]>([]);
   const [totalPages, setTotalPages] = useState(0);
 
   const [isLoading, setIsLoading] = useState(true);
   const [error, setError] = useState(false);
-
-  useEffect(() => {
-    setPage(0);
-    setIsLoading(true);
-  }, [uid]);
 
   useEffect(() => {
     const getNews = async () => {
@@ -28,8 +25,14 @@ export const useGetNewsList = (uid: string) => {
       }
     };
 
-    getNews();
-  }, [uid, page]);
+    if (uid !== prevUid) {
+      setPage(0);
+      setPrevUid(uid);
+      setIsLoading(true);
+    } else {
+      getNews();
+    }
+  }, [uid, prevUid, page]);
 
   return { page, setPage, newsList, totalPages, isLoading, error };
 };
